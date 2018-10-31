30e586b000436974ed74924acfa564597cf264c1
Anil Kumar <anil.ajax@gmail.com>
2018-10-31 12:22:36 +0530

    Factory.php

---
+++ Factory.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Factory design pattern example
+ * 
+ * @author Anil Kumar (https://github.com/anil-ajax)
+ * This code follow PSR standards (https://github.com/anil-ajax/psr-standards)
+ */
+
+
+class Car
+{
+    private $model;
+    private $color;
+
+    public function __construct($model, $color)
+    {
+        $this->model = $model;
+        $this->color = $color;
+    }
+
+    public function getDetails()
+    {
+        return $this->model . ' ' . $this->color;
+    }
+}
+
+class CarFactory
+{
+    public static function create($model, $color)
+    {
+        return new Automobile($model, $color);
+    }
+}
+
+$maruti = CarFactory::create('Dezire', 'Red');
+
+var_dump($maruti->getDetails()); 
+
