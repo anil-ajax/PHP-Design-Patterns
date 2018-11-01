96aba2326bd57e2116ae94c9837d73bc929fabb6
Anil Kumar <anil.ajax@gmail.com>
2018-11-01 12:22:42 +0530

    Strategy Pattern Example

---
+++ Strategy.php
@@ -0,0 +1,22 @@
+<?php
+
+interface OutputInterface
+{
+    public function load();
+}
+
+class SerializedArrayOutput implements OutputInterface
+{
+    public function load()
+    {
+        return serialize($arrayOfData);
+    }
+}
+
+class JsonStringOutput implements OutputInterface
+{
+    public function load()
+    {
+        return json_encode($arrayOfData);
+    }
+}
